f108b50c2af0afdad60b455e1b17ccadf0fbb3d3
Juliano <julianosoder1989@gmail.com>
2022-11-12 13:46:35 -0300

    Mais um usuario

--- options.js
+++ options.js
@@ -8,6 +8,11 @@ const Julianos = {
     senha: 'soder2022'
 }
 
+const Teste = {
+    name: 'Teste',   
+    senha: 'teste123'
+}
+
 function validaLogin(nome, senha){
     if(nome == 'Tiago'){
         localStorage.setItem('login', JSON.stringify(Tiago));
@@ -16,6 +21,10 @@ function validaLogin(nome, senha){
     if(nome == 'Juliano'){
         localStorage.setItem('login', JSON.stringify(Julianos));
         return senha == Julianos.senha;
+    }
+    if(nome == 'Teste'){
+        localStorage.setItem('login', JSON.stringify(Teste));
+        return senha == Teste.senha;
     }
     return false;
 }
