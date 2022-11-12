e58f994b3dced30cfc8cbcc7b7211b7965d89d74
Juliano <julianosoder1989@gmail.com>
2022-11-12 15:38:41 -0300

    pre deploy changes

--- options.js
+++ options.js
@@ -1,8 +1,3 @@
-const Tiago = {
-    name: 'Tiago',
-    senha: 'tiago2022'
-}
-
 const Julianos = {
     name: 'Juliano',   
     senha: 'soder2022'
@@ -14,10 +9,6 @@ const Teste = {
 }
 
 function validaLogin(nome, senha){
-    if(nome == 'Tiago'){
-        localStorage.setItem('login', JSON.stringify(Tiago));
-        return senha == Tiago.senha;
-    }
     if(nome == 'Juliano'){
         localStorage.setItem('login', JSON.stringify(Julianos));
         return senha == Julianos.senha;
